da5caa1d65168290a240179bd298c38017479442
Cebans <julestrodriguez@uniboyaca.edu.co>
2022-05-30 12:14:04 -0500

    ...

--- eliminar.php
+++ eliminar.php
@@ -3,32 +3,33 @@
 require_once dirname(__DIR__) . '/estudiantes_app/db/conexion_db.php';
 require_once dirname(__DIR__) . '/estudiantes_app/controllers/i_controller.php';
 require_once dirname(__DIR__) . '/estudiantes_app/models/estudiante.php';
-require_once dirname(__DIR__) . '/estudiantes_app/controllers/Estudiantes_controller.php';
+require_once dirname(__DIR__) . '/estudiante_app/controllers/estudiantes_controller.php';
 
 use controllers\EstudianteController;
-$estudianteController = new EstudianteController();
+
+$estudianteController = new EstudiantesController();
 $id = empty($_GET['idE']) ? 0 : $_GET['idE'];
-$resultado = $estudianteController ->delete($id);
+$resultado = $estudianteController->delete($id);
 ?>
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <title>Registro</title>
-    </head>
-    <body>
-        <h1>Resultados de la operacion</h1>
-        <?php
-        if($resultado){
-            echo '<p>Se elimino el registro.</p>';
-        }else{
-            echo '<p>Se presento un error al guardar los datos.Vuleva a intentar.</p>';
-        }
-        echo '<br>';
-        echo '<a href=index.php">Volver a la lista<a/>';
-        ?>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Registro</title>
+</head>
+
+<body>
+    <h1>Resultados de la operacion</h1>
+    <?php
+    if ($resultado) {
+        echo '<p>Se elimino el registro.</p>';
+    } else {
+        echo '<p>Se presento un error al guardar los datos.Vuleva a intentar.</p>';
+    }
+    echo '<br>';
+    echo '<a href=index.php">Volver a la lista<a/>';
+    ?>
+</body>
+
 </html>
-
-
-
