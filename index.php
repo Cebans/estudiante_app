06839b847af38306cecbdf59218fc4d6d65be06f
Cebans <julestrodriguez@uniboyaca.edu.co>
2022-05-30 20:35:20 -0500

    arreglo

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once dirname(__DIR__) . '/estudiantes_app/controllers/i_controller.php';
 require_once dirname(__DIR__) . '/estudiantes_app/models/estudiantes.php';
 require_once dirname(__DIR__) . '/estudiantes_app/controllers/estudiante_controller.php';
 
-use controllers\EstudiantesController;
+use controllers\EstudianteController;
 
 $estudianteController = new EstudianteController();
 
